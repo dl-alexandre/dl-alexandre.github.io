67b917fa097280a646fe9481ba7db6396a991c52
Dalton Alexandre <166029845+dl-alexandre@users.noreply.github.com>
2024-07-03 08:21:36 -0700

    PWA

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Change this to your repository name
+var GHPATH = '/dl-alexandre.github.io';
+// Other settings...
+var URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/index.html`,
+    `${GHPATH}/css/styles.css`,
+    `${GHPATH}/js/app.js`
+];
